1f8ab911a435817d658e4ae2d38ff1661f1637ea
SiNa <sina885jaleb@gmail.com>
2026-08-07 03:37:51 +0330

    feat(like): use signal for update total like

---
+++ MySite/blog/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
+from .models import Post
+
+@receiver(m2m_changed, sender=Post.likes.through)
+def like_signal(sender, instance, **kwargs):
+    instance.total_likes = instance.likes.count()
+    instance.save()
