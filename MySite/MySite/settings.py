b99b8caaad037aa925b2b9ff434c05b950c22253
SiNa <sina885jaleb@gmail.com>
2026-08-11 02:49:28 +0330

    feat(smtp): Add smtp gmail to project

--- MySite/MySite/settings.py
+++ MySite/MySite/settings.py
@@ -151,5 +151,9 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
