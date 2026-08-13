331c6aa47c689c99aebc5a75e45a425cba08aee3
SiNa <sina885jaleb@gmail.com>
2026-08-13 03:44:12 +0330

    feat(ticket): update status when admin responds
    - Add signal for ticket response
    - Update ticket status after admin response

--- MySite/blog/signals.py
+++ MySite/blog/signals.py
@@ -1,8 +1,18 @@
-from django.db.models.signals import m2m_changed
+from django.db.models.signals import m2m_changed, pre_save
 from django.dispatch import receiver
-from .models import Post
+from .models import Post, Ticket
 
 @receiver(m2m_changed, sender=Post.likes.through)
 def like_signal(sender, instance, **kwargs):
     instance.total_likes = instance.likes.count()
     instance.save()
+
+@receiver(pre_save, sender=Ticket)
+def update_ticket_status(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    old_ticket = Ticket.objects.get(id=instance.id)
+
+    if old_ticket.answer != instance.answer:
+        instance.status = Ticket.Status.CHECK
